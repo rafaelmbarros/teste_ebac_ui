d2824989f7a3c09eccb70b5876ae0d07861b6fbb
Rafael Monteiro de Barros <rafaelmonteirodebarros@12677-NB.local>
2022-01-24 15:54:45 -0300

    add afterEach

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -5,6 +5,10 @@ context('Funcionalidade login', () => {
     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/');
   });
 
+  afterEach(() => {
+    cy.screenshot();
+  });
+
   it('Deve fazer login com sucesso', () => {
     cy.get('#username').type('aluno_ebac@teste.com');
     cy.get('#password').type('teste@teste.com');
